0fd57e8d43366307d367c5d67dc866837f4f88d8
Tedi <xhuli.tedi@gmail.com>
2022-03-18 17:43:56 +0100

    laravel project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+
+@section('title','About Us')
+
+
+@section('content')
+<div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+    <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+        <h1>About Us</h1>
+    </div>
+
+    <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+
+        <p>This is About page</p>
+    </div>
+
+@endsection
+
+
+</div>
